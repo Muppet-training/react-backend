6acb67dcf35a99705f6fab4fcb6540020cfd6627
Fed-up <tomcurphey12@gmail.com>
2018-05-27 13:02:49 +1000

    /api/users on port 5000 working

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/api/users", function(req, res, next) {
   res.json([
     { id: 1, username: "somebody" },
     { id: 2, username: "somebody else" }
