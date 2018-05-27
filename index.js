43e269f6a481b5e957b3edb5f782be055aca11ea
Fed-up <tomcurphey12@gmail.com>
2018-05-27 16:37:04 +1000

    cleaned up project

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
+const express = require("express");
+const path = require("path");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +34,9 @@ app.use(function(err, req, res, next) {
 });
 
 // We have set the app to listen to PORT 3001 in the package,json file
-// app.listen(5000, () => console.log("Express server started at port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
 
 module.exports = app;
